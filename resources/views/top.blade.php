9a1f4622a920973a2b9fdc931711a956bb336021
nodake <qxx27768@gmail.com>
2020-09-15 12:42:28 +0900

    自作スタイルとレイアウト先を追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,94 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.template')
+@section('title','TOP')
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
-
-
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        .container {
-            margin-top: 50px;
-        }
-
-        .card {
-            margin-bottom: 15px;
-        }
-
-        .card-body {
-            display: flex;
-            position: relative;
-        }
-
-        .btn {
-
-            position: absolute;
-            right: 0.5rem;
-            bottom: 0.5rem;
-
-        }
-    </style>
+    <!-- 自作css -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 
 
@@ -126,17 +41,17 @@
                     ->orderByRaw('id DESC')
                     ->get();
 
-                   
+
                 foreach ($items as $item) {
                     $path = $item->path;
                     $read_path = str_replace('public/', 'storage/', $path);
                     $id = $item->id;
                     $title = $item->tittle;
-                    
+
                     $price = $item->price;
 
                     $text = $item->text;
-                   
+
                     echo <<<ENDF
                    <div class="col-lg-4">
                     <div class="card">
