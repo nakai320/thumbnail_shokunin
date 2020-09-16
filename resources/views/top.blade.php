e0a21e379452a04730b5795045ac2989ecb0d78b
m.nakai <nakawest7@yahoo.co.jp>
2020-09-16 13:03:54 +0900

    ロゴ追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -66,7 +66,7 @@
         </a>
     </div> -->
 
-    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <a href="/item/$id"><img src="images/sample.jpg" alt="サムネイル画像"></a>
@@ -76,54 +76,54 @@
                     <p>ここに職人の説明？</p>
                 </div>
             </div>
-                <?php
-                foreach ($items as $item) {
-                    $path = $item->path;
-                    $id = $item->id;
-                    $read_path = str_replace('public/', 'storage/', $path);
-                    echo <<< TP_CAL
+            <?php
+            foreach ($items as $item) {
+                $path = $item->path;
+                $id = $item->id;
+                $read_path = str_replace('public/', 'storage/', $path);
+                echo <<< TP_CAL
 
                 <div class="carousel-item">
                     <a href="/item/$id"><img class="card-img-top" src="$read_path" alt="サムネイル画像"></a>
-                    画像内キャプション
+                 
                     <div class="carousel-caption d-none d-md-block">
                         <h5>ここに職人の名前</h5>
                         <p>ここに職人の説明？</p>
                     </div>
                 </div>
 TP_CAL;
-                }
-                ?>
-            </div>
-            </div>
+            }
+            ?>
+        </div>
+    </div>
 
 
 
-            <!-- container -->
-            <div class="container">
-                <h1>サムネイル一覧</h1>
-                <div class="container">
-                    <div class="row">
-                        <?php
-                        // $user = Auth::id();
+    <!-- container -->
+    <div class="container">
+        <h1>サムネイル一覧</h1>
+        <div class="container">
+            <div class="row">
+                <?php
+                // $user = Auth::id();
 
-                        // $items = DB::table('uploads')
-                        //     ->orderByRaw('id DESC')
-                        //     // ->get();
-                        //     // ->paginate(3);
-                        //     ->simplePaginate(3);
+                // $items = DB::table('uploads')
+                //     ->orderByRaw('id DESC')
+                //     // ->get();
+                //     // ->paginate(3);
+                //     ->simplePaginate(3);
 
-                        foreach ($items as $item) {
-                            $path = $item->path;
-                            $read_path = str_replace('public/', 'storage/', $path);
-                            $id = $item->id;
-                            $title = $item->tittle;
+                foreach ($items as $item) {
+                    $path = $item->path;
+                    $read_path = str_replace('public/', 'storage/', $path);
+                    $id = $item->id;
+                    $title = $item->tittle;
 
-                            $price = $item->price;
+                    $price = $item->price;
 
-                            $text = $item->text;
+                    $text = $item->text;
 
-                            echo <<<ENDF
+                    echo <<<ENDF
                    <div class="col-lg-4">
                     <div class="card">
                         <a href="/item/$id"><img class="card-img-top" src="$read_path" alt="サムネイル画像"></a>
@@ -135,15 +135,15 @@ TP_CAL;
                 </div> </div>
                             
 ENDF;
-                        }
+                }
 
 
-                        ?>
-                    </div>
-                </div>
+                ?>
             </div>
         </div>
-        {{ $items->links() }}
+    </div>
+    </div>
+    {{ $items->links() }}
     </div>
     @endsection
     @include('layouts.footer')
