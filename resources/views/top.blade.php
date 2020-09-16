3a847cfe1fa5338b5973a32c7dad8d23586d6436
nodake <qxx27768@gmail.com>
2020-09-16 12:07:02 +0900

    スライダー

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,45 +2,130 @@
 
 <head>
     @section('title','TOP')
-    <!-- 自作フロントcss -->
+    <!-- 自作フロントcss/js -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <script src="{{asset('js/style.js')}}"></script>
 </head>
 
 <body>
     @include('layouts.header')
     @section('content')
-    <div class="tp-main-vew">
-        <div>
+
+    <!-- <div class="tp-main-vew">
+        <div class="tp-main-tt">
             <h1>サムショク</h1>
             <p>サムショクはYouTuberとサムネイル職人をつなぐサイトです。<br>
                 仲介手数料０円！お好きなサムネイルがあったら、職人に直接連絡を取ることができます。</p>
         </div>
-    </div>
-    <!-- container -->
-    <div class="container">
-        <h1>サムネイル一覧</h1>
-        <div class="container">
-            <div class="row">
+    </div> -->
+
+    <!-- <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/images/sample.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ここに職人の名前</h5>
+                    <p>ここに職人の説明？</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/images/sample1.jpg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ここに職人の名前</h5>
+                    <p>ここに職人の説明？</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/images/sample2.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ここに職人の名前</h5>
+                    <p>ここに職人の説明？</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/images/sample3.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ここに職人の名前</h5>
+                    <p>ここに職人の説明？</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div> -->
+
+
+
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <a href="/item/$id"><img src="images/sample.jpg" alt="サムネイル画像"></a>
+                <!-- 画像内キャプション -->
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ここに職人の名前</h5>
+                    <p>ここに職人の説明？</p>
+                </div>
+            </div>
                 <?php
-                // $user = Auth::id();
-
-                // $items = DB::table('uploads')
-                //     ->orderByRaw('id DESC')
-                //     // ->get();
-                //     // ->paginate(3);
-                //     ->simplePaginate(3);
-
                 foreach ($items as $item) {
                     $path = $item->path;
-                    $read_path = str_replace('public/', 'storage/', $path);
                     $id = $item->id;
-                    $title = $item->tittle;
+                    $read_path = str_replace('public/', 'storage/', $path);
+                    echo <<< TP_CAL
 
-                    $price = $item->price;
+                <div class="carousel-item">
+                    <a href="/item/$id"><img class="card-img-top" src="$read_path" alt="サムネイル画像"></a>
+                    画像内キャプション
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>ここに職人の名前</h5>
+                        <p>ここに職人の説明？</p>
+                    </div>
+                </div>
+TP_CAL;
+                }
+                ?>
+            </div>
+            </div>
 
-                    $text = $item->text;
 
-                    echo <<<ENDF
+
+            <!-- container -->
+            <div class="container">
+                <h1>サムネイル一覧</h1>
+                <div class="container">
+                    <div class="row">
+                        <?php
+                        // $user = Auth::id();
+
+                        // $items = DB::table('uploads')
+                        //     ->orderByRaw('id DESC')
+                        //     // ->get();
+                        //     // ->paginate(3);
+                        //     ->simplePaginate(3);
+
+                        foreach ($items as $item) {
+                            $path = $item->path;
+                            $read_path = str_replace('public/', 'storage/', $path);
+                            $id = $item->id;
+                            $title = $item->tittle;
+
+                            $price = $item->price;
+
+                            $text = $item->text;
+
+                            echo <<<ENDF
                    <div class="col-lg-4">
                     <div class="card">
                         <a href="/item/$id"><img class="card-img-top" src="$read_path" alt="サムネイル画像"></a>
@@ -51,16 +136,16 @@
                         </div>
                 </div> </div>
                             
-                ENDF;
-                }
+ENDF;
+                        }
 
 
-                ?>
+                        ?>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    @endsection
-    @include('layouts.footer')
+            @endsection
+            @include('layouts.footer')
 </body>
 
 </html>
