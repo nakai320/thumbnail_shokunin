ec2b30d6c6ed54f31e7742a8a5757190ebcd31da
nodake <qxx27768@gmail.com>
2020-09-18 12:24:43 +0900

    test

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,38 +14,38 @@ class SearchController extends Controller
     public function search(Request $request){
         $search = $request->search;
         $user = Auth::id();
-        // if(strpos($search," ")){
+        if(strpos($search," ")){
 
-        //     $search=explode(" ",$search);
-        //     $items=[];
-        //     for ($i=0;$i<$search[$i];$i++) {
+            $search=explode(" ",$search);
+            $items=[];
+            for ($i=0;$i<$search[$i];$i++) {
 
-        //         $items = DB::table('uploads')
-        //         ->where(
-        //             'tittle', 'LIKE', '%' . $search[$i] . '%',
-        //             )->orWhere('price', 'LIKE', '%' . $search[$i] . '%')
-        //             ->orWhere('text', 'LIKE', '%' . $search[$i] . '%')
-        //         ->orderByRaw('id DESC')
-        //         ->paginate(15);
-        //     }
+                $items = DB::table('uploads')
+                ->where(
+                    'tittle', 'LIKE', '%' . $search[$i] . '%',
+                    )->orWhere('price', 'LIKE', '%' . $search[$i] . '%')
+                    ->orWhere('text', 'LIKE', '%' . $search[$i] . '%')
+                    ->orderByRaw('id DESC')
+                    ->paginate(15);
 
-        // }else if(strpos($search,"　")){
-        //     $search =explode("　", $search);
+            }
+        }else if(strpos($search,"　")){
+            $search =explode("　", $search);
 
-        //     $items = DB::table('uploads')
-        //         ->orwhere([
-        //             ['tittle', 'LIKE', '%' . $search[0] . '%'],
-        //             ['tittle', 'LIKE', '%' . $search[1] . '%']
-        //         ])->orWhere([
-        //             ['price', 'LIKE', '%' . $search[0] . '%'],
-        //             ['price', 'LIKE', '%' . $search[1] . '%']
-        //         ])->orWhere([
-        //             ['text', 'LIKE', '%' . $search[0] . '%'],
-        //             ['text', 'LIKE', '%' . $search[1] . '%']
-        //         ])
-        //         ->orderByRaw('id DESC')
-        //         ->paginate(15);
-        // }else{
+            $items = DB::table('uploads')
+                ->orwhere([
+                    ['tittle', 'LIKE', '%' . $search[0] . '%'],
+                    ['tittle', 'LIKE', '%' . $search[1] . '%']
+                ])->orWhere([
+                    ['price', 'LIKE', '%' . $search[0] . '%'],
+                    ['price', 'LIKE', '%' . $search[1] . '%']
+                ])->orWhere([
+                    ['text', 'LIKE', '%' . $search[0] . '%'],
+                    ['text', 'LIKE', '%' . $search[1] . '%']
+                ])
+                ->orderByRaw('id DESC')
+                ->paginate(15);
+        }else{
             // dd($search);
             $items = DB::table('uploads')
         ->where('tittle', 'LIKE', '%'.$search.'%')
@@ -54,7 +54,7 @@ class SearchController extends Controller
         ->orderByRaw('id DESC')
         // ->get();
         ->paginate(15);
-        // }
+        }
 
         return view(
             'top',
