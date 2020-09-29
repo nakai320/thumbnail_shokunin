c981b9cbf4ab8c756b1a48c8ada04a077bca3c07
nodake <qxx27768@gmail.com>
2020-09-29 14:39:07 +0900

    単数ワード検索追加

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,47 +14,44 @@ class SearchController extends Controller
     public function search(Request $request){
         $search = $request->search;
         $user = Auth::id();
-        if(strpos($search," ")){
 
-            $search=explode(" ",$search);
-            $items=[];
-            for ($i=0;$i<$search[$i];$i++) {
+        // 複数ワード検索
+        // if (strpos($search, " ")) {
+        //     $search=explode(" ", $search);
+        //     $items = DB::table('uploads')
+        //   ->orWhere(function ($query) use ($search) {
+        //       foreach ($search as $search) {
+        //         $query->orWhere('tittle', 'LIKE', '%'.$search.'%');
+        //       }
+        //   }) ->orderByRaw('id DESC')
+        //             ->paginate(15);
 
-                $items = DB::table('uploads')
-                ->where(
-                    'tittle', 'LIKE', '%' . $search[$i] . '%',
-                    )->orWhere('price', 'LIKE', '%' . $search[$i] . '%')
-                    ->orWhere('text', 'LIKE', '%' . $search[$i] . '%')
-                    ->orderByRaw('id DESC')
-                    ->paginate(15);
+        // }else if(strpos($search,"　")){
+        //     $search =explode("　", $search);
 
-            }
-        }else if(strpos($search,"　")){
-            $search =explode("　", $search);
+        //     $items = DB::table('uploads')
+        //         ->orwhere([
+        //             ['tittle', 'LIKE', '%' . $search[0] . '%'],
+        //             ['tittle', 'LIKE', '%' . $search[1] . '%']
+        //         ])->orWhere([
+        //             ['price', 'LIKE', '%' . $search[0] . '%'],
+        //             ['price', 'LIKE', '%' . $search[1] . '%']
+        //         ])->orWhere([
+        //             ['text', 'LIKE', '%' . $search[0] . '%'],
+        //             ['text', 'LIKE', '%' . $search[1] . '%']
+        //         ])
+        //         ->orderByRaw('id DESC')
+        //         ->paginate(15);
+        // }else{
+        // }
 
-            $items = DB::table('uploads')
-                ->orwhere([
-                    ['tittle', 'LIKE', '%' . $search[0] . '%'],
-                    ['tittle', 'LIKE', '%' . $search[1] . '%']
-                ])->orWhere([
-                    ['price', 'LIKE', '%' . $search[0] . '%'],
-                    ['price', 'LIKE', '%' . $search[1] . '%']
-                ])->orWhere([
-                    ['text', 'LIKE', '%' . $search[0] . '%'],
-                    ['text', 'LIKE', '%' . $search[1] . '%']
-                ])
-                ->orderByRaw('id DESC')
-                ->paginate(15);
-        }else{
-            // dd($search);
+        // 単数ワード検索
             $items = DB::table('uploads')
         ->where('tittle', 'LIKE', '%'.$search.'%')
         ->orWhere('price', 'LIKE', '%'.$search.'%')
         ->orWhere('text', 'LIKE', '%'.$search.'%')
         ->orderByRaw('id DESC')
-        // ->get();
         ->paginate(15);
-        }
 
         return view(
             'top',
